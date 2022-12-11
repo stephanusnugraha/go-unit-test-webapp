e8c1b96aa97674085c0507bcfc68c0f88ecc3088
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-12-11 19:08:06 +0700

    add test application route & file server static

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,6 +16,8 @@ func (app *application) routes() http.Handler {
 	mux.Get("/", app.Home)
 
 	// static assets
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
