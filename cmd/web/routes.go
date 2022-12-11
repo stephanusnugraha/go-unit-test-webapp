c08b2d424e178e2fe569f3e46bd60f125e6b2948
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-12-11 17:44:24 +0700

    initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// register middleware
+	mux.Use(middleware.Recoverer)
+
+	// register routes
+
+	// static assets
+
+	return mux
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-unit-test-webapp
+
+go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.8 // indirect
