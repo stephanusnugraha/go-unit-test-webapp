b316f313b0b78cbd204ccdb1ba2f103b5e9a78e7
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-12-12 08:32:01 +0700

    setup middleware to get ip

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 
 	// register middleware
 	mux.Use(middleware.Recoverer)
+	mux.Use(app.addIPToContext)
 
 	// register routes
 	mux.Get("/", app.Home)
