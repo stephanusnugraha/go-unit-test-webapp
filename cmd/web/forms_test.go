f48b76a638c944448f0a60dad85797bffbccd4bb
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-12-13 10:42:43 +0700

    add test validation logic

---
+++ cmd/web/forms_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_Has(t *testing.T) {
+	form := NewForm(nil)
+
+	has := form.Has("whatever")
+	if has {
+		t.Error("form shows has field when it should not")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	form = NewForm(postedData)
+
+	has = form.Has("a")
+	if !has {
+		t.Error("form shows does not have field when it should")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := NewForm(r.PostForm)
+
+	form.Required("a", "b", "c")
+
+	if form.Valid() {
+		t.Error("form shows valid when required fields are missing")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	postedData.Add("b", "b")
+	postedData.Add("c", "c")
+
+	r, _ = http.NewRequest("POST", "/whatever", nil)
+	r.PostForm = postedData
+
+	form = NewForm(r.PostForm)
+	form.Required("a", "b", "c")
+	if !form.Valid() {
+		t.Error("shows post does not have required fields, when it does")
+	}
+}
