a864f1537c94d0787c2b474c460a518741a3c885
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-12-19 15:37:24 +0700

    add test auth middleware

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"go-unit-test-webapp/pkg/data"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -72,5 +73,39 @@ func Test_application_ipFromContext(t *testing.T) {
 	// perform the test
 	if !strings.EqualFold("whatever", ip) {
 		t.Error("wong value returned from context")
+	}
+}
+
+func Test_app_auth(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	var tests = []struct {
+		name   string
+		isAuth bool
+	}{
+		{"logged in", true},
+		{"not logged in", false},
+	}
+
+	for _, e := range tests {
+		handleToTest := app.auth(nextHandler)
+		req := httptest.NewRequest("GET", "http://testing", nil)
+		req = addContextAndSessionToRequest(req, app)
+
+		if e.isAuth {
+			app.Session.Put(req.Context(), "user", data.User{ID: 1})
+		}
+		rr := httptest.NewRecorder()
+
+		handleToTest.ServeHTTP(rr, req)
+		if e.isAuth && rr.Code != http.StatusOK {
+			t.Errorf("%s: expected status code of 200 but got %d", e.name, rr.Code)
+		}
+
+		if !e.isAuth && rr.Code != http.StatusTemporaryRedirect {
+			t.Errorf("%s: expected status code of 307 but got %d", e.name, rr.Code)
+		}
 	}
 }
