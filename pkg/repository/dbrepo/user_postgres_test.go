e9c72d06ccee1c9179033204ee272e7deac10eac
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-12-22 13:56:24 +0700

    add test to spin up the image with postgres

--- pkg/repository/dbrepo/user_postgres_test.go
+++ pkg/repository/dbrepo/user_postgres_test.go
@@ -1,12 +1,14 @@
 package dbrepo
 
 import (
-	_ ""
 	"database/sql"
+	"fmt"
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"log"
 	"os"
 	"testing"
 )
@@ -25,7 +27,57 @@ var pool *dockertest.Pool
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	// connect to docker; fail if docker not running
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not connect to docker; is it running? %s", err)
+	}
+	pool = p
 
+	// set up our docker options, specifying the image and so forth
+	opts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "1",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{
+			"5432",
+		},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+	// get a resource (docker image)
+	resource, err := pool.RunWithOptions(&opts)
+	if err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not start resources: %s", err)
+	}
+
+	// start the image and wait until it's ready
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err := sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			log.Println("Error:", err)
+			return err
+		}
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not connect to database: %s", err)
+	}
+
+	// populate the database with empty tables
+
+	// run tests
 	code := m.Run()
+
+	// clean up
 	os.Exit(code)
 }
