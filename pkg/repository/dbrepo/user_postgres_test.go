14049fcba849ccbc6329f89052c98f7df89218ef
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-12-22 13:31:25 +0700

    install module dockertest

---
+++ pkg/repository/dbrepo/user_postgres_test.go
@@ -0,0 +1,31 @@
+package dbrepo
+
+import (
+	_ ""
+	"database/sql"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/ory/dockertest/v3"
+	"os"
+	"testing"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "postgres"
+	dbName   = "user_test"
+	port     = "5435"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
+)
+
+var resource *dockertest.Resource
+var pool *dockertest.Pool
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+
+	code := m.Run()
+	os.Exit(code)
+}
