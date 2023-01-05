3538f669e7992bc6bf0dc45c647886f33d4fb21f
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-01-05 10:24:53 +0700

    add stub handler for API

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,10 +15,21 @@ func (app *application) routes() http.Handler {
 	// mux.Use(app.enableCORS)
 
 	// authentication routes - auth handler, refresh
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh-token", app.refresh)
 
 	// test handler
 
 	// protected routes
+	mux.Route("/users", func(r chi.Router) {
+		// user auth middleware
+
+		r.Get("/", app.allUsers)
+		r.Get("/{userID}", app.getUser)
+		r.Delete("/{userID}", app.deleteUser)
+		r.Put("/", app.insertUser)
+		r.Patch("/", app.updateUser)
+	})
 
 	return mux
 }
