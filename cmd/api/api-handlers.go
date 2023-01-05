306a69b907adb6e098688bae6a902669a50ba4d9
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-01-05 16:18:53 +0700

    implement authenticate handler

--- cmd/api/api-handlers.go
+++ cmd/api/api-handlers.go
@@ -1,17 +1,49 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+)
+
+type Credentials struct {
+	Username string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
 	// read a json payload
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), 401)
+		return
+	}
 
 	// look up the user by email address
+	user, err := app.DB.GetUserByEmail(creds.Username)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	// check password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	// generate tokens
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"), http.StatusUnauthorized)
+		return
+	}
 
 	// send token to user
+	_ = app.writeJSON(w, http.StatusOK, tokenPairs)
 }
 
 func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
