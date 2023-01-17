d115c3f3870b237c551ce281c595b83fdaf1a5c4
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-01-17 16:57:18 +0700

    add test refreshing tokens

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -10,8 +10,8 @@ import (
 	"time"
 )
 
-const jwtTokenExpiry = time.Minute * 15
-const refreshTokenExpiry = time.Hour * 24
+var jwtTokenExpiry = time.Minute * 15
+var refreshTokenExpiry = time.Hour * 24
 
 type TokenPairs struct {
 	Token        string `json:"access_token"`
