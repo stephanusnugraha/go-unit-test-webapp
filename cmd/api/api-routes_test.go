fc25a1eb32257828814877eb8694e0f2b4c5f341
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-01-17 16:04:07 +0700

    implement handler refresh token

---
+++ cmd/api/api-routes_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func Test_app_routes(t *testing.T) {
+	var registered = []struct {
+		route  string
+		method string
+	}{
+		{"/auth", "POST"},
+		{"/refresh-token", "POST"},
+		{"/users/", "GET"},
+		{"/users/{userID}", "GET"},
+		{"/users/{userID}", "DELETE"},
+		{"/users/", "PATCH"},
+		{"/users/", "PUT"},
+	}
+
+	mux := app.routes()
+
+	chiRoutes := mux.(chi.Routes)
+
+	for _, route := range registered {
+		// check to see if the routes exits
+		if !routeExists(route.route, route.method, chiRoutes) {
+			t.Errorf("route %s is not registered", route.route)
+		}
+	}
+}
+
+func routeExists(testRoute, testMethod string, chiRoutes chi.Routes) bool {
+	found := false
+
+	_ = chi.Walk(chiRoutes, func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		if strings.EqualFold(method, testMethod) && strings.EqualFold(route, testRoute) {
+			found = true
+		}
+		return nil
+	})
+
+	return found
+}
